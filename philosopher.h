71b01f3a343ef562fec21f745e23d9702afd6923
Victor <victor.burton@hotmail.fr>
2023-04-12 20:37:09 +0200

    good progress

--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: vburton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 15:46:13 by vburton           #+#    #+#             */
-/*   Updated: 2023/04/07 16:31:05 by vburton          ###   ########.fr       */
+/*   Updated: 2023/04/12 18:08:49 by vburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@
 #	define DEAD -1
 #	define HAS_FORK 1
 #	define NO_FORK 0
+#	define ODD 1
+#	define PAIR 0
+#	define AVAILABLE 1
+#	define UNAVAILABLE 0
+
+typedef struct	s_philo t_philo;
 
 typedef struct	s_philo
 {
@@ -33,8 +39,13 @@ typedef struct	s_philo
 	long t_now;
 	long t_last_meal;
 	int status;
+	int fork;
 	int left_hand;
 	int right_hand;
+	t_philo	*right_neighbor;
+	t_philo	*left_neighbor;
+	pthread_mutex_t mutex;
+	pthread_mutex_t main_mutex;
 }				t_philo;
 
 typedef struct	s_data
