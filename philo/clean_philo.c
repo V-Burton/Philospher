c2e0f0c3d7a60fb475e6cfb851b49ae2c5019ba0
VBurton97 <102577872+VBurton97@users.noreply.github.com>
2023-07-10 14:00:29 +0200

    remove useless check in clean fonction

--- philo/clean_philo.c
+++ philo/clean_philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   clean_philo.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vburton <vburton@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: vburton <vburton@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:41:37 by vburton           #+#    #+#             */
-/*   Updated: 2023/06/12 18:53:34 by vburton          ###   ########.fr       */
+/*   Updated: 2023/07/10 13:45:32 by vburton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,6 @@ void	clear_mutex(t_data *data, t_fork *forks, int nb_threads)
 {
 	int	i;
 
-	i = 0;
-	while (i < nb_threads)
-	{
-		pthread_mutex_lock(&forks[i].in_use);
-		pthread_mutex_unlock(&forks[i].in_use);
-		i++;
-	}
 	i = 0;
 	pthread_mutex_lock(&data->glob);
 	while (i < nb_threads)
